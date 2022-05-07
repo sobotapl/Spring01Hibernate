c60997bdac4713749ba6734f8f2d2071cdc06ff7
sobotapl <piosoboci@gmail.com>
2022-05-07 16:21:54 +0200

    Spring Data JPA task 5 + update all

--- src/main/java/pl/coderslab/controller/SpringDataBookController.java
+++ src/main/java/pl/coderslab/controller/SpringDataBookController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.model.Book;
+import pl.coderslab.model.Publisher;
 import pl.coderslab.repository.BookRepository;
+import pl.coderslab.repository.PublisherRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,11 +20,19 @@ import java.util.List;
 public class SpringDataBookController {
 
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
     @GetMapping("all")
     public void findAll(){
         List<Book> all = bookRepository.findAll();
         all.forEach(b -> log.info("{}", b.getId()));
+    }
+
+    @GetMapping("publisher")
+    public void findByNip(){
+        Optional<Publisher> pb = publisherRepository.findByNip("0");
+        log.info("{}", pb.get().getNip());
+
     }
 
 
